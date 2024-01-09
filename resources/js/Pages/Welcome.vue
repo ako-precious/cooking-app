b13f551ab30e7c5afb16320619343f3f5510b26b
AKO PRECIOUS <akoprecious58@gmail.com>
2024-01-09 13:41:03 -0500

    adding date range

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -45,43 +45,21 @@ import { Head, Link } from "@inertiajs/vue3";
                 class="w-full max-w-xs lg:max-w-lg 2xl:max-w-2xl bg-snow dark:bg-oynx rounded-md hidden lg:flex items-center"
             >
                 <div
-                    class="bg-transparent uppercase font-bold text-sm p-4 mr-4 flex justify-around w-full"
+                    class="bg-transparent capitalize font-bold text-sm p-4 mr-4 flex justify-around w-full"
                     name=""
                     id=""
                 >
                     <a class="text-oynx rounded-full dark:text-snow font-bold cook dark:cook py-2 px-3" href="">
-                        <p>Be a Cook</p>
+                        <p>Meal Schedule</p>
                     </a>
                     <a class="text-oynx rounded-full dark:text-snow font-bold cook dark:cook py-2 px-3" href="">
-                        <p>Be a Cook</p>
+                        <p>Special Meal</p>
                     </a>
                     <a class="text-oynx rounded-full dark:text-snow font-bold cook dark:cook py-2 px-3" href="">
                         <p>Be a Cook</p>
                     </a>
                 </div>
-                <!-- <div class="divcontainer relative w-70/100">
-                    <input
-                        required=""
-                        type="text"
-                        name="text"
-                        class="input w-full h-full p-4"
-                    />
-                    <svg
-                        class="ml-auto h-5 px-4 absolute right-0 top-3 text-gray-500 svg-inline--fa fa-search fa-w-16 fa-9x"
-                        aria-hidden="true"
-                        focusable="false"
-                        data-prefix="far"
-                        data-icon="search"
-                        role="img"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 512 512"
-                    >
-                        <path
-                            fill="currentColor"
-                            d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"
-                        ></path>
-                    </svg>
-                </div> -->
+              
             </div>
 
             <!-- buttons -->
---
+++ resources/js/Pages/daterange.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="relative">
+      <input
+        v-model="startDate"
+        @focus="showPicker = true"
+        @blur="showPicker = false"
+        placeholder="Start Date"
+        class="w-32 py-2 px-4 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+      />
+      <input
+        v-model="endDate"
+        @focus="showPicker = true"
+        @blur="showPicker = false"
+        placeholder="End Date"
+        class="w-32 py-2 px-4 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+      />
+  
+      <div v-show="showPicker" class="absolute top-12 left-0 z-10 bg-white p-4 border border-gray-300 rounded">
+        <!-- Your date range picker content goes here -->
+        <p>Select start and end dates</p>
+        <input type="date" v-model="startDate" />
+        <input type="date" v-model="endDate" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        startDate: '',
+        endDate: '',
+        showPicker: false,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add any additional styling here */
+  </style>
+  
