c5df671ddc6b96c5053ac53ff69fda7328df0a47
AKO PRECIOUS <akoprecious58@gmail.com>
2024-01-11 16:20:26 -0500

    factory for meal

--- database/factories/MealFactory.php
+++ database/factories/MealFactory.php
@@ -2,6 +2,10 @@
 
 namespace Database\Factories;
 
+
+use App\Models\Cook;
+use App\Models\User;
+use App\Models\Meal;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +13,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class MealFactory extends Factory
 {
+    protected $model = \App\Models\Meal::class;
     /**
      * Define the model's default state.
      *
@@ -16,8 +21,18 @@ class MealFactory extends Factory
      */
     public function definition(): array
     {
+         $foods = [
+            "Pizza", "Pasta", "Tacos", "Salad", "Soup", "Burger", "Sushi", "Fried Rice",
+            "Steak", "Chicken", "Fish", "Fruit Salad", "Sandwich", "Wrap", "Smoothie",
+            "Ice Cream", "Cake", "Cookies", "Donuts", "Pie", "Pancakes", "Waffles",
+            "Oatmeal", "Cereal", "Eggs", "Bacon", "Toast", "Bagel", "Yogurt"
+          ];
+          $cooksIds = Cook::pluck('user_id')->all();
         return [
-            //
+                       'cook_id' => $this->faker->randomElement($cooksIds),
+            'name' => $this->faker->randomElement($foods),
+            'description' => implode(' ', $this->faker->sentences(3)),
         ];
+       
     }
 }
