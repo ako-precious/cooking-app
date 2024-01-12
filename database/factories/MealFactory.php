2788144f2fcfe333c0ff58393b18dca1aac211c9
AKO PRECIOUS <akoprecious58@gmail.com>
2024-01-11 19:24:58 -0500

    update meal factory

--- database/factories/MealFactory.php
+++ database/factories/MealFactory.php
@@ -21,18 +21,22 @@ class MealFactory extends Factory
      */
     public function definition(): array
     {
-         $foods = [
-            "Pizza", "Pasta", "Tacos", "Salad", "Soup", "Burger", "Sushi", "Fried Rice",
-            "Steak", "Chicken", "Fish", "Fruit Salad", "Sandwich", "Wrap", "Smoothie",
-            "Ice Cream", "Cake", "Cookies", "Donuts", "Pie", "Pancakes", "Waffles",
-            "Oatmeal", "Cereal", "Eggs", "Bacon", "Toast", "Bagel", "Yogurt"
-          ];
-          $cooksIds = Cook::pluck('user_id')->all();
+        $foods = [
+            'Pizza', 'Pasta', 'Tacos', 'Salad', 'Sushi', 'Fried Rice','Cake', 'Toast and egg',
+            'Sushi', 'Pizza Margherita', 'Pad Thai', 'Tacos', 'Croissant', 'Hamburger', 'Paella', 
+            'Miso Soup', 'Goulash', 'Churros', 'Kimchi', 'Poutine', 'Ceviche', 'Dim Sum', 'Bruschetta', 
+            'Steak', 'Chicken', 'Fish', 'Fruit Salad', 'Sandwich and Yogurt', 'Wrap', 'Smoothie and Donuts',
+            'Moussaka', 'Empanadas', 'Samosa', 'Peking Duck', 'Tiramisu', 'Wiener Schnitzel', 'Borscht', 'Croque Monsieur',
+            'Jollof Rice', 'Fish and Chips', 'Pierogi', 'Falafel', 'Pho', 'Rendang', 'Pasta Carbonara', 'Burger and Ice Cream', 
+        ];
+       
+//   "", , "Cookies", "", "Pie", "Pancakes", "Waffles",
+//   "Oatmeal", "Cereal", "", "Bacon", , "Bagel", ""
+        $cooksIds = Cook::pluck('user_id')->all();
         return [
-                       'cook_id' => $this->faker->randomElement($cooksIds),
+            'cook_id' => $this->faker->randomElement($cooksIds),
             'name' => $this->faker->randomElement($foods),
             'description' => implode(' ', $this->faker->sentences(3)),
         ];
-       
     }
 }
