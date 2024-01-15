7d69d5cdbbbcde0d14a4ad7091f1eab79c38ba8b
AKO PRECIOUS <akoprecious58@gmail.com>
2024-01-14 23:16:07 -0500

    adding setting button

---
+++ resources/js/Layouts/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- Navbar -->
+    
+    <!-- class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all ease-in shadow-none duration-250 rounded-2xl lg:flex-nowrap lg:justify-start sticky top-[1%] backdrop-saturate-200 backdrop-blur-2xl dark:bg-slate-850/80 dark:shadow-dark-blur bg-[hsla(0,0%,100%,0.8)] shadow-blur z-110" -->
+   
+    <nav class="notfixeddiv" navbar-main navbar-scroll="false">
+        <div
+            class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit"
+        >
+           <slot>Default</slot>
+
+          
+           
+        </div>
+    </nav>
+
+    <!-- end Navbar -->
+</template>
+<script setup>
+import { Link } from "@inertiajs/vue3";
+defineProps(["message"]);
+</script>
+<script></script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -10,23 +10,20 @@ import Welcome from '@/Components/Welcome.vue';
 </template> -->
 
 <template>
-   
+    <div
+        class="absolute h-screen w-full bg-[linear-gradient(to_right_bottom,rgba(5,150,105,1),rgba(74,223,128,0.8))] dark:hidden min-h-75"
+    ></div>
     <!-- sidenav  -->
 
-    <SideNav
-        @mouseover="hoverSidebar()"
-        @mouseout="hoverOutSidebar()"
-        :class="
-            (sidebarReduced,
-            {
-                'bg-slate-850': isClassWhite,
-                'translate-x-0': isClassTranslated,
-            })
-        "
+    <SideNav @mouseover="hoverSidebar()" @mouseout="hoverOutSidebar()"
+        :class=" sidebarReduced, {
+            'bg-slate-850': isClassWhite,
+            'translate-x-0':isClassTranslated,
+            
+        }"
     >
         <SideBarHeader>
-            <span
-                :class="{ 'opacity-0': isLinkHidden }"
+            <span :class="{ 'opacity-0': isLinkHidden }"
                 class="ml-2 font-bold transition-all duration-200 ease-in-out text-2xl uppercase"
             >
                 Cricket
@@ -35,83 +32,221 @@ import Welcome from '@/Components/Welcome.vue';
 
         <SideBarCollapse>
             <li class="mt-0.5 w-full group">
-                <SideBarLink :class="getRouteClass('default')">
-                    <SideBarIcon class="group-hover:animate-bounce">
-                        </SideBarIcon>
-                    <SideBarSpan :class="{ 'opacity-0': isLinkHidden }">
-                        Dashboard
-                    </SideBarSpan>
-                </SideBarLink>
+                <Link :href="`/`">
+                    <SideBarLink :class="{ 'activesidelink': isDashboard }"  >
+                        <SideBarIcon class="group-hover:animate-bounce">
+                            <font-awesome-icon
+                                icon="fa-solid fa-house"
+                                style="color: #2a744a"
+                        /></SideBarIcon>
+                        <SideBarSpan :class="{ 'opacity-0': isLinkHidden }"> Dashboard </SideBarSpan>
+                    </SideBarLink>
+                </Link>
             </li>
+
+          
+             
             <li class="mt-0.5 w-full group">
-                <SideBarLink :class="getRouteClass('players')">
-                    <SideBarIcon class="group-hover:animate-pulse">
-                       
-                    </SideBarIcon>
-                    <SideBarSpan :class="{ 'opacity-0': isLinkHidden }">
-                        Players
-                    </SideBarSpan>
-                </SideBarLink>
-            </li>
-            <li class="mt-0.5 w-full group">
+                <Link :href="`/teams`">
                 <SideBarLink :class="getRouteClass('teams')">
-                    <SideBarIcon class="group-hover:animate-spin">
-                        
-                    </SideBarIcon>
-                    <SideBarSpan :class="{ 'opacity-0': isLinkHidden }">
-                        Teams
-                    </SideBarSpan>
-                </SideBarLink>
-            </li>
-            <li class="mt-0.5 w-full group">
-                <SideBarLink :class="getRouteClass('batting')">
-                    <SideBarIcon class="group-hover:animate-spin">
+                    <SideBarIcon class="group-hover:animate-pulse">
+                        <font-awesome-icon icon="fa-solid fa-people-group" style="color: #2a744a"/>
                        
                     </SideBarIcon>
-                    <SideBarSpan :class="{ 'opacity-0': isLinkHidden }">
-                        Batting
-                    </SideBarSpan>
+                    <SideBarSpan :class="{ 'opacity-0': isLinkHidden }"> Teams </SideBarSpan>
                 </SideBarLink>
+            </Link>
             </li>
             <li class="mt-0.5 w-full group">
-                <SideBarLink :class="getRouteClass('bowling')">
-                    <SideBarIcon class="group-hover:animate-spin">
-                        
+                <Link :href="`/players`">
+                <SideBarLink :class="getRouteClass('players')">
+                    <SideBarIcon class="group-hover:animate-bounce">
+   
+   <div class="`"></div>                     <font-awesome-icon icon="fa-solid fa-person"  style="color: #2a744a"/> 
+                            
                     </SideBarIcon>
-                    <SideBarSpan :class="{ 'opacity-0': isLinkHidden }">
-                        Bowling
-                    </SideBarSpan>
+                    <SideBarSpan :class="{ 'opacity-0': isLinkHidden }"> Players </SideBarSpan>
                 </SideBarLink>
+            </Link>
+            </li>
+            
+            <li class="mt-0.5 w-full group">
+                <Link :href="`/battings`">
+                <SideBarLink :class="getRouteClass('batting')">
+                    <SideBarIcon class="group-hover:animate-pulse">
+                        <font-awesome-icon
+                            icon="fas-solid fa-baseball-bat-ball"
+                            style="color: #2a744a"
+                        />
+                    </SideBarIcon>
+                    <SideBarSpan :class="{ 'opacity-0': isLinkHidden }"> Batting Stats </SideBarSpan>
+                </SideBarLink>
+            </Link>
+            </li>
+            <li class="mt-0.5 w-full group">
+                <Link :href="`/bowlings`">
+                <SideBarLink :class="getRouteClass('bowlings')">
+                    <SideBarIcon class="group-hover:animate-spin">
+                        <font-awesome-icon
+                            icon="fa-solid fa-baseball"
+                            style="color: #2a744a"
+                        />
+                    </SideBarIcon>
+                    <SideBarSpan :class="{ 'opacity-0': isLinkHidden }"> Bowling Stats </SideBarSpan>
+                </SideBarLink>
+            </Link>
             </li>
         </SideBarCollapse>
     </SideNav>
     <!-- sidenav  -->
 
-    
+    <main :class="marginReduced"
+        class="relative h-full max-h-screen transition-all duration-200 ease-in-out  rounded-xl"
+    >
+        <!-- Navbar -->
+        <NavBar :class="classChanged">
+            <nav>
+                <ol
+                    class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16"
+                >
+                    <li class="text-sm leading-normal">
+                        <a class="text-white opacity-50" href="javascript:;"
+                            >Pages</a
+                        >
+                    </li>
+                    <li id="resultDiv"
+                        class="text-sm pl-2 capitalize leading-normal text-white before:float-left before:pr-2 before:text-white before:content-['/']"
+                        aria-current="page"
+                    >
+                    {{ resultText }}
+                    </li>
+                </ol>
+                <h6 class="mb-0 font-bold text-white capitalize"> <div id="resultDiv">{{ resultText }}</div> </h6>
+            </nav>
+
+            <SubNavBar @click="minSidebar()">
+                <i  :class="{ 'translate-x-[5px]': isClassTranslated2 }"
+                    class="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all " 
+                ></i>
+                <i
+                    class="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all"
+                ></i>
+                <i  :class="{ 'translate-x-[5px]': isClassTranslated2 }"
+                    class="ease relative block h-0.5 rounded-sm bg-white transition-all "
+                ></i>
+            </SubNavBar>
+
+            <NavSideBar>
+                
+                <div class="w-4.5 overflow-hidden" @click="openSidebar()">
+
+                    <i
+                        class="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all"
+                        :class="{ 'translate-x-[5px]': isClassTranslated }"
+                    ></i>
+                    <i
+                        class="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all"
+                    ></i>
+                    <i
+                        class="ease relative block h-0.5 rounded-sm bg-white transition-all"
+                        :class="{ 'translate-x-[5px]': isClassTranslated }"
+                    ></i>
+                </div>
+            </NavSideBar>
+        </NavBar>
+        <!-- end Navbar -->
+
+        <!-- cards -->
+        <div class="w-full px-6 py-6 mx-auto">
+            <slot>Default</slot>
+
+            <!-- footer -->
+            <!-- <Footer /> -->
+            <!-- end footer -->
+        </div>
+        <!-- end cards -->
+    </main>
     <!-- fixed plugin  -->
-    
+    <FixLayout @click="toggleNavClass()">
+        <div>
+            <h6 class="mb-0 dark:text-white">Sidebar Colors</h6>
+        </div>
+        <a href="javascript:void(0)">
+            <div class="my-2 text-left" sidenav-colors>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-blue-500 to-violet-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-slate-700 text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    active-color
+                    data-color="blue"
+                    onclick="sidebarColor(this)"
+                ></span>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    data-color="gray"
+                    onclick="sidebarColor(this)"
+                ></span>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-blue-700 to-cyan-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    data-color="cyan"
+                    onclick="sidebarColor(this)"
+                ></span>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-emerald-500 to-teal-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    data-color="emerald"
+                    onclick="sidebarColor(this)"
+                ></span>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-orange-500 to-yellow-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    data-color="orange"
+                    onclick="sidebarColor(this)"
+                ></span>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-red-600 to-orange-600 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    data-color="red"
+                    onclick="sidebarColor(this)"
+                ></span>
+            </div>
+        </a>
+        <hr
+            class="h-px mx-0 my-1 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent"
+        />
+
+        <!-- Sidenav Type -->
+        <div class="mt-4">
+            <h6 class="mb-0 dark:text-white">Sidenav Type</h6>
+            <p class="text-sm leading-normal dark:text-white dark:opacity-80">
+                Choose between 2 different sidenav types.
+            </p>
+        </div>
+        <div class="flex">
+            <WhiteBgBtn class="whitebtn" @click="changeBgWhite()"
+                >Light</WhiteBgBtn
+            >
+            <WhiteBgBtn class="blackbtn" @click="changeBgBlack()"
+                >Dark</WhiteBgBtn
+            >
+        </div>
+    </FixLayout>
     <!-- end fixed plugin  -->
 </template>
 <script setup>
-// import { Link } from "@inertiajs/vue3";
-
+import { Link } from "@inertiajs/vue3";
+// import Footer from "@/Layouts/FooterLayout.vue";
 import SideBarLink from "@/Components/Sidebar/SideBarLink.vue";
 import SideBarSpan from "@/Components/Sidebar/SideBarSpan.vue";
 import SideBarIcon from "@/Components/Sidebar/SideBarIcon.vue";
 import SideBarHeader from "@/Components/Sidebar/SideBarHeader.vue";
-// import SideBarColour from "@/Layout/Component/Sidebar/SideBarColour.vue";
 import SideBarCollapse from "@/Components/Sidebar/SideBarCollapse.vue";
+import NavBar from "@/Layouts/NavBar.vue";
+import SubNavBar from "@/Components/Navbar/NavBar.vue";
+import NavSideBar from "@/Components/Navbar/NavSideBar.vue";
 import SideNav from "@/Layouts/SideNav.vue";
-
+import FixLayout from "@/Layouts/FixLayout.vue";
 // defineProps(["message"]);
 </script>
 <script>
+
 export default {
-    computed: {
-        currentRoute() {
-            return window.location.pathname;
-        },
-    },
+
     data() {
         return {
             classChanged: "notfixeddiv",
@@ -123,7 +258,12 @@ export default {
             isClassTranslated: false,
             isNavClassTranslated: false,
             isClassTranslated2: true,
+            isDashboard: false,
+            resultText: 'Dashboard', 
         };
+    },
+    created() {
+        this.updateTextBasedOnFilePath(window.location.pathname);
     },
     methods: {
         toggleNavClass() {
@@ -146,49 +286,69 @@ export default {
         },
         minSidebar() {
             this.isLinkHidden = !this.isLinkHidden;
-            this.marginReduced =
-                this.marginReduced === "xl:ml-68" ? "xl:ml-30" : "xl:ml-68";
-            this.sidebarReduced =
-                this.sidebarReduced === "sidebar" ? "minisidebar" : "sidebar";
+            this.marginReduced = this.marginReduced === "xl:ml-68" ? "xl:ml-30" : "xl:ml-68";
+            this.sidebarReduced = this.sidebarReduced === "sidebar"
+                    ? "minisidebar"
+                    : "sidebar";
             this.isClassTranslated2 = !this.isClassTranslated2;
         },
         hoverSidebar() {
-            if (this.sidebarReduced === "minisidebar") {
+            if (this.sidebarReduced ===  "minisidebar") {
+                
                 this.isLinkHidden = !this.isLinkHidden;
-                this.marginReduced =
-                    this.marginReduced === "xl:ml-68" ? "xl:ml-30" : "xl:ml-68";
-                this.sidebarReduced =
-                    this.sidebarReduced === "sidebar"
+                this.marginReduced = this.marginReduced === "xl:ml-68" ? "xl:ml-30" : "xl:ml-68";
+                this.sidebarReduced = this.sidebarReduced === "sidebar"
                         ? "minisidebar"
                         : "sidebar";
                 this.isClassTranslated2 = !this.isClassTranslated2;
-                this.hover = true;
+                this.hover = true
             }
         },
         hoverOutSidebar() {
-            if (this.hover === true) {
+            if ( this.hover === true) {
+                
                 this.isLinkHidden = !this.isLinkHidden;
-                this.marginReduced =
-                    this.marginReduced === "xl:ml-68" ? "xl:ml-30" : "xl:ml-68";
-                this.sidebarReduced =
-                    this.sidebarReduced === "sidebar"
+                this.marginReduced = this.marginReduced === "xl:ml-68" ? "xl:ml-30" : "xl:ml-68";
+                this.sidebarReduced = this.sidebarReduced === "sidebar"
                         ? "minisidebar"
                         : "sidebar";
                 this.isClassTranslated2 = !this.isClassTranslated2;
-                this.hover = false;
+                this.hover = false
             }
         },
-        // isPlayersRoute() {
-        //     // Check if the route name contains the word "players"
-        //     return currentRoute.includes("players");
-        // },
-        getRouteClass() {
-            return (routeName) => {
-                return {
-                    activateclass: this.currentRoute.includes(routeName),
-                };
-            };
+        getRouteClass(routeName) {
+                var currentRoute =   window.location.pathname;
+                if(currentRoute === '/'){
+                    
+                    return this.isDashboard = true         
+                }else{
+
+                    return {
+                        activesidelink: currentRoute.includes(routeName),
+                    };
+                }
+
+            
         },
+        
+
+
+        updateTextBasedOnFilePath( filePath ) {
+        
+
+            if (filePath.includes("players")) {
+                this.resultText = "Players";
+            } else if (filePath.includes("teams")) {
+                this.resultText = "Teams";
+            } else if (filePath.includes("batting")) {
+                this.resultText = "Batting";
+            } else if (filePath.includes("bowling")) {
+                this.resultText = "Bowling";
+            } 
+        },
+
+        
     },
 };
 </script>
+
