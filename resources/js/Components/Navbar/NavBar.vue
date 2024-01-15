6fbad2369b9c6643ec3a8e660aaad9d792427c8d
AKO PRECIOUS <akoprecious58@gmail.com>
2024-01-14 23:00:05 -0500

    adind the nav

---
+++ resources/js/Components/Navbar/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex items-center">
+        <a
+            mini-sidenav-burger=""
+            href="javascript:;"
+            class="hidden p-0 text-sm text-white transition-all ease-nav-brand lg:block"
+            aria-expanded="false"
+        >
+            <div class="w-4.5 overflow-hidden">
+               
+                <slot>Default</slot>
+            </div>
+        </a>
+    </div>
+</template>
+<script setup></script>
+<script></script>
