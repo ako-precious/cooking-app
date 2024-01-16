c6b5218431cc1a7cb554035c674cc34fc9975739
AKO PRECIOUS <akoprecious58@gmail.com>
2024-01-16 13:27:49 -0500

    change the nav style

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -8,6 +8,18 @@ import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 
+// ? import Footer from "@/Layouts/FooterLayout.vue";
+import SideBarLink from "@/Components/Sidebar/SideBarLink.vue";
+import SideBarSpan from "@/Components/Sidebar/SideBarSpan.vue";
+import SideBarIcon from "@/Components/Sidebar/SideBarIcon.vue";
+import SideBarHeader from "@/Components/Sidebar/SideBarHeader.vue";
+// import SideBarCollapse from "@/Components/Sidebar/SideBarCollapse.vue";
+import NavBar from "@/Layouts/NavBar.vue";
+import SubNavBar from "@/Components/Navbar/NavBar.vue";
+import NavSideBar from "@/Components/Navbar/NavSideBar.vue";
+import SideNav from "@/Layouts/SideNav.vue";
+import FixLayout from "@/Layouts/FixLayout.vue";
+
 defineProps({
     title: String,
 });
@@ -33,27 +45,121 @@ const logout = () => {
 
         <Banner />
 
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            <nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-                <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
+        <div
+        class="absolute h-screen w-full bg-[linear-gradient(to_right_bottom,rgba(5,150,105,1),rgba(74,223,128,0.8))] dark:hidden min-h-75"
+    ></div>
+    <!-- sidenav  -->
+
+    <SideNav @mouseover="hoverSidebar()" @mouseout="hoverOutSidebar() " class=" scroll-m-0 scroll "
+        :class=" sidebarReduced, {
+            'bg-slate-850': isClassWhite,
+            'translate-x-0':isClassTranslated,
+            
+        }"
+    >
+        <SideBarHeader>
+             <!-- Logo -->
+             <div class="shrink-0 flex items-center">
                                 <Link :href="route('dashboard')">
                                     <ApplicationMark class="block h-9 w-auto" />
                                 </Link>
                             </div>
 
-                            <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
-                            </div>
-                        </div>
+            <span :class="{ 'opacity-0': isLinkHidden }"
+                class="ml-2 font-bold transition-all duration-200 ease-in-out text-2xl uppercase"
+            >
+                Cricket
+            </span>
+        </SideBarHeader>
 
+        <SideBarCollapse>
+            <li class="mt-0.5 w-full group">
+                <Link :href="`/`">
+                    <SideBarLink :class="{ 'activesidelink': isDashboard }"  >
+                        <SideBarIcon class="group-hover:animate-bounce">
+                            <font-awesome-icon
+                                icon="fa-solid fa-house"
+                                style="color: #2a744a"
+                        /></SideBarIcon>
+                        <SideBarSpan :class="{ 'opacity-0': isLinkHidden }"> Dashboard </SideBarSpan>
+                    </SideBarLink>
+                </Link>
+            </li>
+
+          
+             
+            <li class="mt-0.5 w-full group">
+                <Link :href="`/teams`">
+                <SideBarLink :class="getRouteClass('teams')"  class="  transition-all duration-200 delay-75">
+                    <SideBarIcon class="group-hover:text-polynesian dark:group-hover:text-lighred">
+                       
+                        <font-awesome-icon icon="calendar" />
+                    </SideBarIcon>
+                    <SideBarSpan :class="{ 'opacity-0': isLinkHidden }" class="group-hover:text-polynesian dark:group-hover:text-lighred"> Calendar </SideBarSpan>
+                </SideBarLink>
+            </Link>
+            </li>
+           
+        </SideBarCollapse>
+    </SideNav>
+    <!-- sidenav  -->
+
+    <main :class="marginReduced"
+        class="relative h-full max-h-screen transition-all duration-200 ease-in-out  rounded-xl"
+    >
+        <!-- Navbar -->
+        <NavBar :class="classChanged">
+            <nav>
+                <ol
+                    class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16"
+                >
+                    <li class="text-sm leading-normal">
+                        <a class="text-white opacity-50" href="javascript:;"
+                            >Pages</a
+                        >
+                    </li>
+                    <li id="resultDiv"
+                        class="text-sm pl-2 capitalize leading-normal text-white before:float-left before:pr-2 before:text-white before:content-['/']"
+                        aria-current="page"
+                    >
+                    {{ resultText }}
+                    </li>
+                </ol>
+                <h6 class="mb-0 font-bold text-white capitalize"> <div id="resultDiv">{{ resultText }}</div> </h6>
+            </nav>
+
+            <SubNavBar @click="minSidebar()">
+                <i  :class="{ 'translate-x-[5px]': isClassTranslated2 }"
+                    class="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all " 
+                ></i>
+                <i
+                    class="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all"
+                ></i>
+                <i  :class="{ 'translate-x-[5px]': isClassTranslated2 }"
+                    class="ease relative block h-0.5 rounded-sm bg-white transition-all "
+                ></i>
+            </SubNavBar>
+            
+            <NavSideBar>
+                
+                <div class="w-4.5 overflow-hidden" @click="openSidebar()">
+
+                    <i
+                        class="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all"
+                        :class="{ 'translate-x-[5px]': isClassTranslated }"
+                    ></i>
+                    <i
+                        class="ease mb-0.75 relative block h-0.5 rounded-sm bg-white transition-all"
+                    ></i>
+                    <i
+                        class="ease relative block h-0.5 rounded-sm bg-white transition-all"
+                        :class="{ 'translate-x-[5px]': isClassTranslated }"
+                    ></i>
+                </div>
+            </NavSideBar>
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div class="flex justify-between h-16">
+                        
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
                             <div class="ms-3 relative">
                                 <!-- Teams Dropdown -->
@@ -271,6 +377,83 @@ const logout = () => {
                         </div>
                     </div>
                 </div>
+        </NavBar>
+        <!-- end Navbar -->
+
+        <!-- cards -->
+        <div class="w-full px-6 py-6 mx-auto">
+            <slot>Default</slot>
+
+            <!-- footer -->
+            <!-- <Footer /> -->
+            <!-- end footer -->
+        </div>
+        <!-- end cards -->
+    </main>
+    <!-- fixed plugin  -->
+    <FixLayout @click="toggleNavClass()">
+        <div>
+            <h6 class="mb-0 dark:text-white">Sidebar Colors</h6>
+        </div>
+        <a href="javascript:void(0)">
+            <div class="my-2 text-left" sidenav-colors>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-blue-500 to-violet-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-slate-700 text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    active-color
+                    data-color="blue"
+                    onclick="sidebarColor(this)"
+                ></span>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-zinc-800 to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    data-color="gray"
+                    onclick="sidebarColor(this)"
+                ></span>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-blue-700 to-cyan-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    data-color="cyan"
+                    onclick="sidebarColor(this)"
+                ></span>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-emerald-500 to-teal-400 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    data-color="emerald"
+                    onclick="sidebarColor(this)"
+                ></span>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-orange-500 to-yellow-500 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    data-color="orange"
+                    onclick="sidebarColor(this)"
+                ></span>
+                <span
+                    class="py-2.2 text-xs rounded-circle h-5.6 mr-1.25 w-5.6 ease-in-out bg-gradient-to-tl from-red-600 to-orange-600 relative inline-block cursor-pointer whitespace-nowrap border border-solid border-white text-center align-baseline font-bold uppercase leading-none text-white transition-all duration-200 hover:border-slate-700"
+                    data-color="red"
+                    onclick="sidebarColor(this)"
+                ></span>
+            </div>
+        </a>
+        <hr
+            class="h-px mx-0 my-1 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent"
+        />
+
+        <!-- Sidenav Type -->
+        <div class="mt-4">
+            <h6 class="mb-0 dark:text-white">Sidenav Type</h6>
+            <p class="text-sm leading-normal dark:text-white dark:opacity-80">
+                Choose between 2 different sidenav types.
+            </p>
+        </div>
+        <div class="flex">
+            <WhiteBgBtn class="whitebtn" @click="changeBgWhite()"
+                >Light</WhiteBgBtn
+            >
+            <WhiteBgBtn class="blackbtn" @click="changeBgBlack()"
+                >Dark</WhiteBgBtn
+            >
+        </div>
+    </FixLayout>
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            <nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+                <!-- Primary Navigation Menu -->
+                
             </nav>
 
             <!-- Page Heading -->
@@ -287,3 +470,112 @@ const logout = () => {
         </div>
     </div>
 </template>
+
+<script>
+
+export default {
+
+    data() {
+        return {
+            classChanged: "notfixeddiv",
+            isClassWhite: false,
+            isLinkHidden: false,
+            hover: false,
+            marginReduced: "xl:ml-68",
+            sidebarReduced: "sidebar",
+            isClassTranslated: false,
+            isNavClassTranslated: false,
+            isClassTranslated2: true,
+            isDashboard: false,
+            resultText: 'Dashboard', 
+        };
+    },
+    created() {
+        this.updateTextBasedOnFilePath(window.location.pathname);
+    },
+    methods: {
+        toggleNavClass() {
+            // Toggle the value when the button is clicked
+
+            this.classChanged =
+                this.classChanged === "notfixeddiv"
+                    ? "fixeddiv"
+                    : "notfixeddiv";
+        },
+        changeBgBlack() {
+            this.isClassWhite = true;
+        },
+        changeBgWhite() {
+            this.isClassWhite = false;
+        },
+        openSidebar() {
+            this.isNavClassTranslated = !this.isNavClassTranslated;
+            this.isClassTranslated = !this.isClassTranslated;
+        },
+        minSidebar() {
+            this.isLinkHidden = !this.isLinkHidden;
+            this.marginReduced = this.marginReduced === "xl:ml-68" ? "xl:ml-30" : "xl:ml-68";
+            this.sidebarReduced = this.sidebarReduced === "sidebar"
+                    ? "minisidebar"
+                    : "sidebar";
+            this.isClassTranslated2 = !this.isClassTranslated2;
+        },
+        hoverSidebar() {
+            if (this.sidebarReduced ===  "minisidebar") {
+                
+                this.isLinkHidden = !this.isLinkHidden;
+                this.marginReduced = this.marginReduced === "xl:ml-68" ? "xl:ml-30" : "xl:ml-68";
+                this.sidebarReduced = this.sidebarReduced === "sidebar"
+                        ? "minisidebar"
+                        : "sidebar";
+                this.isClassTranslated2 = !this.isClassTranslated2;
+                this.hover = true
+            }
+        },
+        hoverOutSidebar() {
+            if ( this.hover === true) {
+                
+                this.isLinkHidden = !this.isLinkHidden;
+                this.marginReduced = this.marginReduced === "xl:ml-68" ? "xl:ml-30" : "xl:ml-68";
+                this.sidebarReduced = this.sidebarReduced === "sidebar"
+                        ? "minisidebar"
+                        : "sidebar";
+                this.isClassTranslated2 = !this.isClassTranslated2;
+                this.hover = false
+            }
+        },
+        getRouteClass(routeName) {
+                var currentRoute =   window.location.pathname;
+                if(currentRoute === '/'){
+                    
+                    return this.isDashboard = true         
+                }else{
+
+                    return {
+                        activesidelink: currentRoute.includes(routeName),
+                    };
+                }
+
+            
+        },
+        
+
+
+        updateTextBasedOnFilePath( filePath ) {
+        
+
+            if (filePath.includes("players")) {
+                this.resultText = "Players";
+            } else if (filePath.includes("teams")) {
+                this.resultText = "Teams";
+            } else if (filePath.includes("batting")) {
+                this.resultText = "Batting";
+            } else if (filePath.includes("bowling")) {
+                this.resultText = "Bowling";
+            } 
+        },
+
+        
+    },
+};
+</script>
