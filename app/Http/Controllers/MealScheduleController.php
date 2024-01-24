9c9f1b64b8a2f1acc9bd8420837d459bb516e839
AKO PRECIOUS <akoprecious58@gmail.com>
2024-01-23 23:55:38 -0500

    j

--- app/Http/Controllers/MealScheduleController.php
+++ app/Http/Controllers/MealScheduleController.php
@@ -10,7 +10,7 @@ class MealScheduleController extends Controller
     public function index()
     {
         // dd(Players::all());
-        // dd((MealSchedule::all() ));
+        dd((MealSchedule::with('meal')->get() ));
         
         // return Inertia::render('Meal-Schedule/Index', ['InitialEvent' => MealSchedule::all()]);
         return inertia('Meal-Schedule/Index', ['InitialEvent' => MealSchedule::with('meal')->get()]);
