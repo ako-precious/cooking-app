e7271a301ae7fcefd90c7633237e8171b71b1a47
AKO PRECIOUS <akoprecious58@gmail.com>
2024-02-03 00:49:56 -0500

    pass messages

--- resources/js/Layouts/FoodCard.vue
+++ resources/js/Layouts/FoodCard.vue
@@ -1,9 +1,7 @@
-
-
 <script setup>
 import axios from "axios";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
- import Login from "@/Pages/Auth/Login.vue"; 
+import Login from "@/Pages/Auth/Login.vue";
 import TextInput from "@/Components/TextInput.vue";
 defineProps(["meal"]);
 </script>
@@ -12,12 +10,13 @@ export default {
     inheritAttrs: false,
     data() {
         return {
-            userId: '' ,
-            message:'',
+            userId: "",
+            message: "",
+            error: "",
             newSchedule: {
                 meal_name: "",
                 meal_time: "",
-                user_id:  "",
+                user_id: "",
                 start_date: "",
                 end_date: "",
             },
@@ -27,7 +26,11 @@ export default {
         };
     },
     created() {
-        this.getMeals(), this.handleScroll(), this.filterMeals(), this.fetchData(), this.closeModal();
+        this.getMeals(),
+            this.handleScroll(),
+            this.filterMeals(),
+            this.fetchData(),
+            this.closeModal();
     },
     methods: {
         getMeals() {
@@ -84,17 +87,17 @@ export default {
         openModal(meal) {
             // clear everything in the div and close it
             this.newEventModalVisible = true;
-           if (this.$page.props.auth.user) {
-             // this.suggestedMeal = [];
-             this.newSchedule = {
-                 meal_name: meal.title,
-                 meal_id: meal.id.toString(),
-                 user_id: this.$page.props.auth.user.id.toString(),
-                 start_date: "",
-                 end_date: "",
-                 meal_time: "Choose a Meal time",
-             };
-           }
+            if (this.$page.props.auth.user) {
+                // this.suggestedMeal = [];
+                this.newSchedule = {
+                    meal_name: meal.title,
+                    meal_id: meal.id.toString(),
+                    user_id: this.$page.props.auth.user.id.toString(),
+                    start_date: "",
+                    end_date: "",
+                    meal_time: "Choose a Meal time",
+                };
+            }
         },
         closeModal() {
             // clear everything in the div and close it
@@ -105,8 +108,10 @@ export default {
                 end_date: "",
                 meal_time: "",
             };
+            this.message = "";
+            this.error = "";
         },
-        addSchedule(){
+        addSchedule() {
             this.formattedEvents = {
                 meal_id: this.newSchedule.meal_id,
                 user_id: this.newSchedule.user_id,
@@ -115,18 +120,27 @@ export default {
                 end_date: this.newSchedule.end_date,
             };
             axios
-            .post("/schedule", this.formattedEvents)
+                .post("/schedule", this.formattedEvents)
                 .then((resp) => {
-                    this.message = resp.message
+                    this.message = resp.message;
                     console.log(resp);
 
-                    new Promise((resolve) => setTimeout(resolve, 100));
-
-                    this.closeModal();
-                    // this.addingMode = !this.addingMode;
+                    setTimeout(() => {
+                        this.closeModal();
+                        // Uncomment the line below if you want to toggle addingMode after the delay
+                        // this.addingMode = !this.addingMode;
+                    }, 5000);
                 })
-                .catch((err) => console.log("Unable to add Meal !", err));
-         },
+                .catch((err) => {
+                    this.error = "Unable to add Meal !";
+                    setTimeout(() => {
+                        this.error = "";
+                        console.log("Unable to add Meal !", err);
+                        // Uncomment the line below if you want to toggle addingMode after the delay
+                        // this.addingMode = !this.addingMode;
+                    }, 5000);
+                });
+        },
     },
 };
 </script>
@@ -150,12 +164,15 @@ export default {
                 <h5
                     class="text-lg font-bold tracking-tight text-oynx dark:text-snow text-nowrap"
                 >
-                   {{ meal.title }}
+                    {{ meal.title }}
                 </h5>
             </a>
             <div class="my-1 flex items-center justify-between">
                 <a href="#" class=" ">
-                    <span class="text-xs text-oynx dark:text-snow hover:font-bold ">{{ meal.cook.name }}</span>
+                    <span
+                        class="text-xs text-oynx dark:text-snow hover:font-bold"
+                        >{{ meal.cook.name }}</span
+                    >
                 </a>
                 <div class="flex items-center">
                     <font-awesome-icon icon="star" class="text-persian" />
@@ -179,129 +196,144 @@ export default {
                         class="text-xl pr-2 text-oynx active:text-persian hover:text-polynesian dark:text-snow dark:hover:text-lighred"
                     />
 
-                    <font-awesome-icon  @click="openModal(meal)"
-                        icon="circle-plus" 
+                    <font-awesome-icon
+                        @click="openModal(meal)"
+                        icon="circle-plus"
                         class="text-xl px-2 text-oynx active:text-persian hover:text-polynesian dark:text-snow dark:hover:text-lighred"
                     />
                     <font-awesome-icon
                         icon="share"
-                        class="text-xl px-2 text-oynx active:text-persian hover:text-polynesian dark:text-snow  dark:hover:text-lighred"
+                        class="text-xl px-2 text-oynx active:text-persian hover:text-polynesian dark:text-snow dark:hover:text-lighred"
                     />
                 </div>
             </div>
         </div>
     </div>
-    <div     
-            class="modal disable-scrollbars overflow-y-auto overflow-x-hidden fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-[100] flex justify-center items-center backdrop-blur-sm w-full h-full"
-            v-show="newEventModalVisible"
+    <div
+        class="modal disable-scrollbars overflow-y-auto overflow-x-hidden pt-16 fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-[100] flex justify-center items-center backdrop-blur-sm w-full h-full"
+        v-show="newEventModalVisible"
+    >
+        <div
+            class="relative p-4 w-full max-w-md max-h-full transition-all duration-300 ease-in delay-200"
         >
-            <div 
-                class="relative p-4 w-full max-w-md max-h-full transition-all duration-300 ease-in delay-200"
-            >
-                <div class="relative  rounded-lg shadow shadow-small">
-                    <button
-                        @click="closeModal"
-                        type="button"
-                        class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-12 h-12 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                        data-modal-hide="popup-modal"
-                    >
-                        <font-awesome-icon
-                            icon="fa-solid fa-close"
-                            class="text-persian text-2xl"
-                        />
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                    <div v-if="$page.props.auth.user">
-
-                        <form @submit.prevent class="p-4 md:py-8 text-center">
-                            <h2 class="text-oynx dark:text-snow font-bold text-xl">
-                              New Meal Schedule
-                            </h2>
-                            <div class="py-4 relative">
-                                <TextInput readonly
-                                    class="my-2 w-full"
-                                    v-model="newSchedule.meal_name"
-                                    placeholder="Meal Name"
-                                />
-                                <TextInput
-                                    readonly
-                                    hidden
-                                    class="my-2 w-full"
-                                    type="number"
-                                    v-model= newSchedule.meal_id
-                                    placeholder=""
-                                />
-                                <TextInput
-                                    readonly
-                                    hidden
-                                    class="my-2 w-full"
-                                    type="number"
-                                    v-model= newSchedule.user_id
-                                    placeholder=""
-                                />
-                            </div>
-    
-                            <div class="py-4 flex justify-between">
-                                <TextInput
-                                    class="w-[47%]"
-                                    v-model="newSchedule.start_date"
-                                    type="date"
-                                    placeholder=""
-                                />
-                                <TextInput
-                                    class="w-[47%]"
-                                    v-model="newSchedule.end_date"
-                                    type="date"
-                                    placeholder=""
-                                />
-                            </div>
-                            <div class="py-4 flex justify-between">
-                                <select
-                                    v-model="newSchedule.meal_time"
-                                    title="Meal Time" placeholder="Choose a meal time"
-                                    class="border-oynx bg-snow text-oynx dark:bg-oynx dark:text-snow bg-gradient-to-br from-[#e3dedf] to-[#ffffff] w-full shadow-snow-sm dark:bg-gradient-to-br dark:from-[#2b312e] dark:to-[#333a37] focus:shadow-none dark:focus:shadow-none dark:shadow-oynx-sm dark:border-snow focus:border-polynesian dark:focus:border-lighred focus:ring-polynesian dark:focus:ring-lighred rounded-md "
-                                >
-                                    <option  selected                               
-                                        class="bg-snow text-oynx dark:bg-oynx dark:text-snow"                                   
-                                    >  {{ newSchedule.meal_time }}
-                                    </option>
-                                    <option
-                                        class="bg-snow text-oynx dark:bg-oynx dark:text-snow"
-                                        value="breakfast"
-                                    >
-                                    
-                                        Breakfast
-                                    </option>
-                                    <option
-                                        class="bg-snow text-oynx dark:bg-oynx dark:text-snow"
-                                        value="launch"
-                                    >
-                                        Launch
-                                    </option>
-                                    <option
-                                        class="bg-snow text-oynx dark:bg-oynx dark:text-snow"
-                                        value="dinner"
-                                    >
-                                        Dinner
-                                    </option>
-                                </select>
-                            </div>
-                            <div
-                                class="flex justify-center item-center">
-                                <PrimaryButton  @click="addSchedule" class="w-full">Save</PrimaryButton>
-                            </div>
-    
-                           
-                        </form>
+            <div class="relative rounded-lg shadow shadow-small">
+                <button
+                    @click="closeModal"
+                    type="button"
+                    class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-12 h-12 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="popup-modal"
+                >
+                    <font-awesome-icon
+                        icon="fa-solid fa-close"
+                        class="text-persian text-2xl"
+                    />
+                    <span class="sr-only">Close modal</span>
+                </button>
+                <div v-if="$page.props.auth.user">
+                    <div class="" v-if="message">
+                        <div
+                            class="px-6 py-4 mt-1 bg-persian/20 rounded-lg text-persian"
+                        >
+                            <span class="font-bold"> {{ message }} </span>
+                        </div>
                     </div>
-                    <template v-else>
-                        <Login></Login>
-                    </template>
+                    <div class="" v-if="error">
+                        <div
+                            class="px-6 py-4 mt-1 bg-lighred/20 rounded-lg text-lighred"
+                        >
+                             <span class="font-bold">{{ error }}</span> 
+                        </div>
+                        
+                    </div>
+
+                    <form @submit.prevent class="p-4 md:py-8 text-center">
+                        <h2 class="text-oynx dark:text-snow font-bold text-xl">
+                            New Meal Schedule
+                        </h2>
+                        <div class="py-4 relative">
+                            <TextInput
+                                readonly
+                                class="my-2 w-full"
+                                v-model="newSchedule.meal_name"
+                                placeholder="Meal Name"
+                            />
+                            <TextInput
+                                readonly
+                                hidden
+                                class="my-2 w-full"
+                                type="number"
+                                v-model="newSchedule.meal_id"
+                                placeholder=""
+                            />
+                            <TextInput
+                                readonly
+                                hidden
+                                class="my-2 w-full"
+                                type="number"
+                                v-model="newSchedule.user_id"
+                                placeholder=""
+                            />
+                        </div>
+
+                        <div class="py-4 flex justify-between">
+                            <TextInput
+                                class="w-[47%]"
+                                v-model="newSchedule.start_date"
+                                type="date"
+                                placeholder=""
+                            />
+                            <TextInput
+                                class="w-[47%]"
+                                v-model="newSchedule.end_date"
+                                type="date"
+                                placeholder=""
+                            />
+                        </div>
+                        <div class="py-4 flex justify-between">
+                            <select
+                                v-model="newSchedule.meal_time"
+                                title="Meal Time"
+                                placeholder="Choose a meal time"
+                                class="border-oynx bg-snow text-oynx dark:bg-oynx dark:text-snow bg-gradient-to-br from-[#e3dedf] to-[#ffffff] w-full shadow-snow-sm dark:bg-gradient-to-br dark:from-[#2b312e] dark:to-[#333a37] focus:shadow-none dark:focus:shadow-none dark:shadow-oynx-sm dark:border-snow focus:border-polynesian dark:focus:border-lighred focus:ring-polynesian dark:focus:ring-lighred rounded-md"
+                            >
+                                <option
+                                    selected
+                                    class="bg-snow text-oynx dark:bg-oynx dark:text-snow"
+                                >
+                                    {{ newSchedule.meal_time }}
+                                </option>
+                                <option
+                                    class="bg-snow text-oynx dark:bg-oynx dark:text-snow"
+                                    value="breakfast"
+                                >
+                                    Breakfast
+                                </option>
+                                <option
+                                    class="bg-snow text-oynx dark:bg-oynx dark:text-snow"
+                                    value="launch"
+                                >
+                                    Launch
+                                </option>
+                                <option
+                                    class="bg-snow text-oynx dark:bg-oynx dark:text-snow"
+                                    value="dinner"
+                                >
+                                    Dinner
+                                </option>
+                            </select>
+                        </div>
+                        <div class="flex justify-center item-center">
+                            <PrimaryButton @click="addSchedule" class="w-full"
+                                >Save</PrimaryButton
+                            >
+                        </div>
+                    </form>
                 </div>
+                <template v-else>
+                    <Login></Login>
+                </template>
             </div>
-
         </div>
+    </div>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -95,7 +95,7 @@ export default {
     <!-- component -->
     <header
         :class="{ fixed: isHeaderFixed }"
-        class="bg-snow dark:bg-oynx z-990 w-screen transition-all duration-300 delay-75 ease-in animate-fade-in"
+        class="bg-snow dark:bg-oynx z-990 transition-all duration-300 delay-75 ease-in animate-fade-in"
     >
         <Navbar class="bg-snow dark:bg-oynx">
             <template #search>
