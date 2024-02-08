1f6d50ec660264a07e172055fe1c0c0ecc63296d
AKO PRECIOUS <akoprecious58@gmail.com>
2024-02-07 22:46:40 -0500

    chill

--- resources/js/Pages/Cook/Overview.vue
+++ resources/js/Pages/Cook/Overview.vue
@@ -5,43 +5,45 @@ import { Head, Link } from "@inertiajs/vue3";
 <template>
     
         <div class="container relative mx-auto overflow-hidden w-screen">
-            <div class="m-auto flex items-center h-full px-6 lg:p-8">
-                <div class="lg:w-1/2">
-                    <h1 class="font-semibold lg:text-5xl tracking-wide text-oynx dark:text-snow">
+            <div class="m-auto flex flex-col lg:flex-row items-center h-full px-6 lg:p-8">
+                <div class=" lg:w-1/2 py-5">
+                    <h1 class="font-semibold text-3xl lg:text-5xl tracking-wide text-oynx dark:text-snow">
                         Experience simplicity with Foodmart
                     </h1>
                     <!-- <h1 class="font-bold pt-1 lg:text-xl tracking-wide text-oynx dark:text-snow">Get started easily!</h1> -->
                 </div>
                 <div class="lg:w-1/2">
-                    <div class="flex flex-col">
+                    <div class="flex flex-col w-full">
                         <div class="flex pb-5">
                            
-                            <div>
-                                <h1 class="font-bold pt-1 lg:text-2xl tracking-wide text-oynx dark:text-snow">What delicious meal are you planning to cook?</h1>
+                            <div class="w-2/3">
+                                <h1 class="font-bold pt-1 text-xl lg:text-2xl tracking-wide text-oynx dark:text-snow">What delicious meal are you planning to cook?</h1>
                                 <p class=" pt-3  lg:text-lg text-oynx dark:text-snow">Share some basic info, like what you can cook and how many people you can cook for.</p>
                             </div>
-                            <div class="lg:w-1/2 lg:h-[120px]">
-                                
+                            <div class="w-1/3 lg:h-[180px] ml-8 p-3">
+                                <img class="" src="/images/whattocook.jpg" alt="">
+                            </div>
+                     </div>
+                        
+                    
+                        <div class="flex py-5">
+                            
+                            <div class="w-2/3">
+                                <h1 class="font-bold pt-1 text-xl lg:text-2xl tracking-wide text-oynx dark:text-snow">Steal the show</h1>
+                                <p class=" pt-3 lg:text-lg  text-oynx dark:text-snow">Bring your meal to life! Upload 5+ photos, tell your story with a title and description. <br> We'll lend a hand.</p>
+                            </div>
+                            <div class="w-1/3 lg:h-[180px] ml-8 p-3">
+                                <img src="/images/takepicturesofmeal.jpg" alt="">
                             </div>
                      </div>
                         <div class="flex py-5">
                         
-                            <div>
-                                <h1 class="font-bold pt-1 lg:text-2xl tracking-wide text-oynx dark:text-snow">Steal the show</h1>
-                                <p class=" pt-3 lg:text-lg  text-oynx dark:text-snow">Bring your meal to life! Upload 5+ photos, tell your story with a title and description, and we'll lend a hand.</p>
-                            </div>
-                            <div class="lg:w-1/2 lg:h-[120px]">
-                                
-                            </div>
-                     </div>
-                        <div class="flex py-5">
-                        
-                            <div>
-                                <h1 class="font-bold pt-1 lg:text-2xl tracking-wide text-oynx dark:text-snow">Let's get the meal out there!</h1>
+                            <div class="w-2/3">
+                                <h1 class="font-bold pt-1 text-xl lg:text-2xl tracking-wide text-oynx dark:text-snow">Let's get the meal out there!</h1>
                                 <p class=" pt-3 lg:text-lg  text-oynx dark:text-snow">Pick your available time, set the price, and share your culinary creation.</p>
                             </div>
-                            <div class="lg:w-1/2 lg:h-[120px]">
-                                
+                            <div class="w-1/3 lg:h-[180px] ml-8 p-3">
+                                <img src="/images/getitout.jpg" alt="">
                             </div>
                      </div>
                     </div>
