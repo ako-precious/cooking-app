4cd72fc3e0e5687608f5fd11c43a5cae2dc70ac9
AKO PRECIOUS <akoprecious58@gmail.com>
2024-02-16 21:12:45 -0500

    h

--- resources/js/Components/Table/TableRow.vue
+++ resources/js/Components/Table/TableRow.vue
@@ -1,30 +1,35 @@
 <script setup>
+import { Head, Link } from "@inertiajs/vue3";
 defineProps(["meal"]);
+
+
 </script>
 <template>
     <td v-if="meal.name" class="whitespace-nowrap px-6 py-4 font-semibold">
-        {{ meal.name }}
+        <Link :href="`/become-a-cook/${meal.id}/ingredients`">
+            {{ meal.name }}
+        </Link>
     </td>
     <td v-else class="whitespace-nowrap px-6 py-4 font-semibold">
-        Meal created on {{ FormattedDate(meal.created_at) }}
+        <Link :href="`/become-a-cook/${meal.id}/ingredients`">
+            Meal created on {{ FormattedDate(meal.created_at) }}
+        </Link>
     </td>
     <td class="whitespace-nowrap px-6 py-4">{{ meal.status }}</td>
     <td class="whitespace-nowrap px-6 py-4">$ {{ meal.price }}</td>
-    <td class="whitespace-nowrap px-6 py-4 text-center">{{ meal.ordering_preferences }}</td>
-    <td class="whitespace-nowrap px-6 py-4">{{ truncatedIng(meal.ingredients)  }}</td>
-    <td class="whitespace-nowrap px-6 py-4">Cell</td>
-    <td class="whitespace-nowrap px-6 py-4">Cell</td>
+    <td class="whitespace-nowrap px-6 py-4 text-center">
+        {{ meal.ordering_preferences }}
+    </td>
     <td class="whitespace-nowrap px-6 py-4">Cell</td>
 </template>
 
 <script>
 export default {
     created() {
-        this.FormattedDate(); this.truncatedIng()
+        this.FormattedDate();
+        this.truncatedIng();
     },
-    computed: {
-    
-  },
+    computed: {},
     methods: {
         FormattedDate(timestamp) {
             const date = new Date(timestamp);
@@ -36,11 +41,10 @@ export default {
                 .padStart(2, "0")}`;
             return formattedDate;
         },
-        
+
         truncatedIng(description) {
             // Check if description exists and has more than 30 characters
-            if (description &&
-                description.length > 1) {
+            if (description && description.length > 1) {
                 // Truncate description to 30 characters and add ellipsis
                 return description.slice(0, 1) + "...";
             } else {
