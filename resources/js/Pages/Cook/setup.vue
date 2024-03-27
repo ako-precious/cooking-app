135f104e4061b4cf9f99332b381b57b5fb7e807e
AKO PRECIOUS <akoprecious58@gmail.com>
2024-03-26 21:35:07 -0400

    n

--- resources/js/Pages/Cook/setup.vue
+++ resources/js/Pages/Cook/setup.vue
@@ -11,13 +11,11 @@ import { Head, Link } from "@inertiajs/vue3";
         <div
             class="container relative mx-auto overflow-hidden h-screen w-screen"
         >
-            <!-- <iframe
+            <iframe
                 src="https://my.spline.design/untitled-22ece4253c983291af7e559585416b64/"
                 frameborder="0"
-                width="100%"
-                height="100%"
-                class="absolute  -right-40 z-0"
-            ></iframe> -->
+                class="absolute -right-30 w-screen h-screen z-0"
+            ></iframe>
 
             <div class="m-auto flex items-center h-full px-6 lg:p-8">
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
@@ -39,9 +37,100 @@ import { Head, Link } from "@inertiajs/vue3";
                         <Link :href="`/become-a-cook/overview`">
                             <button
                                 @click="addUserToCook"
-                                class="bg-gradient-to-br from-[#e3dedf] to-[#ffffff] shadow-snow-sm dark:shadow-oynx-sm mt-5 button type1 text-xs"
+                                class="bg-gradient-to-br from-[#e3dedf] to-[#ffffff] dark:bg-gradient-to-br dark:from-[#2b312e] dark:to-[#333a37] shadow-snow-sm dark:shadow-oynx-sm mt-5 button type1 text-xs"
                             ></button>
                         </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container relative mx-auto">
+        <div class="grid grid-rows-6 grid-flow-col gap-6 aspect-w-1 aspect-h-1">
+            <div class="row-span-4 ...">
+                <div class="card h-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                            d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"
+                        ></path>
+                    </svg>
+                    <div class="card__content">
+                        <p class="card__title">Card Title</p>
+                        <p class="card__description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-start-2 col-end-4 row-span-2 ...">
+                <div class="card h-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                            d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"
+                        ></path>
+                    </svg>
+                    <div class="card__content">
+                        <p class="card__title">Card Title</p>
+                        <p class="card__description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco.
+                        </p>
+                    </div>
+                </div></div>
+            <div class="col-start-2 col-end-3 row-span-2 ...">
+                <div class="card h-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                            d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"
+                        ></path>
+                    </svg>
+                    <div class="card__content">
+                        <p class="card__title">Card Title</p>
+                        <p class="card__description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco.
+                        </p>
+                    </div>
+                </div></div>
+            <div class="row-span-2 col-span-2 ...">
+                <div class="card h-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                            d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"
+                        ></path>
+                    </svg>
+                    <div class="card__content">
+                        <p class="card__title">Card Title</p>
+                        <p class="card__description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco.
+                        </p>
+                    </div>
+                </div></div>
+            <div class="row-start-3 row-end-7 ...">
+                <div class="card h-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <path
+                            d="M20 5H4V19L13.2923 9.70649C13.6828 9.31595 14.3159 9.31591 14.7065 9.70641L20 15.0104V5ZM2 3.9934C2 3.44476 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.44495 22 3.9934V20.0066C22 20.5552 21.5447 21 21.0082 21H2.9918C2.44405 21 2 20.5551 2 20.0066V3.9934ZM8 11C6.89543 11 6 10.1046 6 9C6 7.89543 6.89543 7 8 7C9.10457 7 10 7.89543 10 9C10 10.1046 9.10457 11 8 11Z"
+                        ></path>
+                    </svg>
+                    <div class="card__content">
+                        <p class="card__title">Card Title</p>
+                        <p class="card__description">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -50,39 +139,103 @@ import { Head, Link } from "@inertiajs/vue3";
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-    data(){
-        return{
+    data() {
+        return {
             newCook: {
                 user_id: this.$page.props.auth.user.id,
                 certificate: null,
                 means_of_id: null,
                 other_info: null,
-                status: 'pending',
-            },}
-    }, 
+                status: "pending",
+            },
+        };
+    },
     methods: {
         addUserToCook() {
-            axios.post('/cook', this.newCook )
-                .then(response => {
+            axios
+                .post("/cook", this.newCook)
+                .then((response) => {
                     // Handle the response and extract the ID of the newly created row
-                   console.log(response.data.id);
+                    console.log(response.data.id);
 
                     // Redirect to the new route with the ID included
                     // this.$router.push(`/become-a-cook/${newMealId}/about-your-meal`);
                 })
-                .catch(error => {
-                    console.error('Error creating new row:', error);
+                .catch((error) => {
+                    console.error("Error creating new row:", error);
                 });
-        }
-    }
-}
+        },
+    },
+};
 </script>
 <!-- Saving for later -->
 <!-- "Indulge your taste buds and unlock a world of culinary delights with our platform, where passionate home chefs transform ordinary ingredients into extraordinary experiences. From tantalizing dishes to mouthwatering desserts, our community of food artisans invites you to savor their creations and bring the flavors of the world into your home. Whether you're seeking a gourmet feast for a special occasion or craving everyday comfort food made with love, discover your perfect match among our talented cooks who are ready to turn your cravings into unforgettable culinary adventures." -->
 
 <style scoped>
+.card {
+    position: relative;
+    /* width: 300px;
+  height: 200px; */
+    background-color: #f2f2f2;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    perspective: 1000px;
+    box-shadow: 0 0 0 5px #ffffff80;
+    transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.card svg {
+    width: 48px;
+    fill: #333;
+    transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.card:hover {
+    transform: scale(1.05);
+    box-shadow: 0 8px 16px rgba(255, 255, 255, 0.2);
+}
+
+.card__content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    box-sizing: border-box;
+    background-color: #f2f2f2;
+    transform: rotateX(-90deg);
+    transform-origin: bottom;
+    transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.card:hover .card__content {
+    transform: rotateX(0deg);
+}
+
+.card__title {
+    margin: 0;
+    font-size: 24px;
+    color: #333;
+    font-weight: 700;
+}
+
+.card:hover svg {
+    scale: 0;
+}
+
+.card__description {
+    margin: 10px 0 0;
+    font-size: 14px;
+    color: #777;
+    line-height: 1.4;
+}
+
 .bg-dots-darker {
     background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
 }
@@ -110,7 +263,7 @@ export default {
     content: "Monetize Your Passion";
     height: 50px;
     width: 165px;
-    background-color: #1B998B;
+    background-color: #1b998b;
     color: #fff;
     position: absolute;
     top: 0%;
@@ -127,7 +280,7 @@ export default {
     height: 50px;
     width: 165px;
     /* background-color: #fff; */
-    color: #1B998B;
+    color: #1b998b;
     position: absolute;
     top: 0%;
     left: 0%;
