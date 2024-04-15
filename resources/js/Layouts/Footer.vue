0f8ad2d46447827e246417afe228e46467358676
AKO PRECIOUS <akoprecious58@gmail.com>
2024-04-15 17:56:13 -0400

    working on footer

--- resources/js/Layouts/Footer.vue
+++ resources/js/Layouts/Footer.vue
@@ -1,14 +1,35 @@
+<script>
+
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(), // Get the current year using Date object
+      appName: 'Ounjemi'  // Get the current year using Date object
+    };
+  },
+  mounted() {
+    // Assign the current year to the data property once the component is mounted
+    this.currentYear = new Date().getFullYear();
+    window.env = {
+        APP_NAME: '{{ env("APP_NAME") }}'
+        // Add other environment variables as needed
+    };
+  }
+};
+
+</script>
+
 <template>
    <!--
   Heads up! 👋
 
 <!-- \ -->
 
-<footer class="bg-white">
+<footer class="border-t border-gray-500 bg-white">
   <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
     <div class="lg:flex lg:items-start lg:gap-8">
       
-      <div class="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16 w-full">
+      <div class="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-4 lg:gap-y-16 w-full">
         <!-- <div class="col-span-2">
           <div>
             <h2 class="text-2xl font-bold text-gray-900">Get the latest news!</h2>
@@ -25,7 +46,7 @@
             <label for="UserEmail" class="sr-only"> Email </label>
 
             <div
-              class="border border-gray-100 p-2 focus-within:ring sm:flex sm:items-center sm:gap-4"
+              class="border border-gray-500 p-2 focus-within:ring sm:flex sm:items-center sm:gap-4"
             >
               <input
                 type="email"
@@ -44,11 +65,11 @@
         </div> -->
 
         <div class="col-span-2 sm:col-span-1">
-          <p class="font-medium text-gray-900">Services</p>
+          <p class="font-medium text-gray-900">Support</p>
 
           <ul class="mt-6 space-y-4 text-sm">
             <li>
-              <a href="#" class="text-gray-700 transition hover:opacity-75"> 1on1 Coaching </a>
+              <a href="#" class="text-gray-700 transition hover:opacity-75">Help </a>
             </li>
 
             <li>
@@ -127,27 +148,29 @@
           </ul>
         </div>
 
-        <div class="col-span-2 sm:col-span-1">
-          <p class="font-medium text-gray-900">Downloads</p>
-
-          <ul class="mt-6 space-y-4 text-sm">
-            <li>
-              <a href="#" class="text-gray-700 transition hover:opacity-75"> Marketing Calendar </a>
-            </li>
-
-            <li>
-              <a href="#" class="text-gray-700 transition hover:opacity-75"> SEO Infographics </a>
-            </li>
-          </ul>
-        </div>
+        
 
         <ul class="col-span-2 flex justify-start gap-6 lg:col-span-5 lg:justify-end">
+          <li>
+            <a 
+              href="#"
+              rel="noreferrer"
+              class="navbar-link p-2"
+            >
+            <font-awesome-icon
+                                    icon="globe"
+                                    class="mr-2"/>
+              <span class="text-sm font-bold" >Eng</span>
+              <span class="sr-only">Language</span>
+
+            </a>
+          </li>
           <li>
             <a
               href="#"
               rel="noreferrer"
               target="_blank"
-              class="text-gray-700 transition hover:opacity-75"
+              class="navbar-link p-2"
             >
               <span class="sr-only">Facebook</span>
 
@@ -166,7 +189,7 @@
               href="#"
               rel="noreferrer"
               target="_blank"
-              class="text-gray-700 transition hover:opacity-75"
+              class="navbar-link p-2"
             >
               <span class="sr-only">Instagram</span>
 
@@ -185,7 +208,7 @@
               href="#"
               rel="noreferrer"
               target="_blank"
-              class="text-gray-700 transition hover:opacity-75"
+              class="navbar-link p-2"
             >
               <span class="sr-only">Twitter</span>
 
@@ -197,50 +220,13 @@
             </a>
           </li>
 
-          <li>
-            <a
-              href="#"
-              rel="noreferrer"
-              target="_blank"
-              class="text-gray-700 transition hover:opacity-75"
-            >
-              <span class="sr-only">GitHub</span>
-
-              <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                <path
-                  fill-rule="evenodd"
-                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="#"
-              rel="noreferrer"
-              target="_blank"
-              class="text-gray-700 transition hover:opacity-75"
-            >
-              <span class="sr-only">Dribbble</span>
-
-              <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                <path
-                  fill-rule="evenodd"
-                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
-          </li>
         </ul>
       </div>
     </div>
 
-    <div class="mt-8 border-t border-gray-100 pt-8">
+    <div class="mt-8 border-t border-gray-500 pt-8">
       <div class="sm:flex sm:justify-between">
-        <p class="text-xs text-gray-500">&copy; 2022. Company Name. All rights reserved.</p>
+        <p class="text-xs text-gray-500">&copy; {{ currentYear }}. {{ appName }} . All rights reserved.</p>
 
         <ul class="mt-8 flex flex-wrap justify-start gap-4 text-xs sm:mt-0 lg:justify-end">
           <li>
@@ -261,3 +247,56 @@
 </footer>
     </template>
   
+  <style>
+.Btn {
+  width: 45px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  background-color: transparent;
+  position: relative;
+  /* overflow: hidden; */
+  border-radius: 7px;
+  cursor: pointer;
+  transition: all .3s;
+}
+
+.svgContainer {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  backdrop-filter: blur(0px);
+  letter-spacing: 0.8px;
+  border-radius: 10px;
+  transition: all .3s;
+  border: 1px solid rgba(156, 156, 156, 0.466);
+}
+
+.BG {
+  position: absolute;
+  content: "";
+  width: 100%;
+  height: 100%;
+  background: #181818;
+  z-index: -1;
+  border-radius: 10px;
+  pointer-events: none;
+  transition: all .3s;
+}
+
+.Btn:hover .BG {
+  transform: rotate(35deg);
+  transform-origin: bottom;
+}
+
+.Btn:hover .svgContainer {
+  background-color: rgba(156, 156, 156, 0.466);
+  backdrop-filter: blur(4px);
+}
+
+</style>
