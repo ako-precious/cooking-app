8345be546d376b4d51b7559b1555a4d4cbdeca88
AKO PRECIOUS <akoprecious58@gmail.com>
2024-04-20 20:27:48 -0400

    mn

--- vite.config.js
+++ vite.config.js
@@ -33,32 +33,37 @@ export default defineConfig({
   ],
 });
 
-// import { defineConfig } from "vite";
-// import laravel from "laravel-vite-plugin";
-// import vue from "@vitejs/plugin-vue";
-
-// VITE_ENV_MODE = production;
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import vue from '@vitejs/plugin-vue';
 
 // export default defineConfig({
-//     mode: 'production',
-    
-//     build: {
-//         mode: "production",
-//         // mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-//     },
-//     plugins: [
-//         laravel({
-//             input: "resources/js/app.js",
-//             refresh: true,
-//         }),
-
-//         vue({
-//             template: {
-//                 transformAssetUrls: {
-//                     base: null,
-//                     includeAbsolute: false,
-//                 },
-//             },
-//         }),
-//     ],
+//     define: {
+//         'import.meta.env.VITE_APP_NAME': JSON.stringify('Ounjemi'),
+//         'import.meta.env.VITE_APP_ENV ': JSON.stringify('production'),
+//         // Other environment variables...
+//       },
+//   mode: 'production', // Set the mode directly to 'production'
+//   build: {
+//     manifest: true,
+//     outDir: 'public/build',
+//     mode: 'production', // Ensure the build mode is also set to 'production'
+//     rollupOptions: {
+//         input: 'resources/js/app.js',
+//     }
+//   },
+//   plugins: [
+//     laravel({
+//       input: 'resources/js/app.js',
+//       refresh: true,
+//     }),
+//     vue({
+//       template: {
+//         transformAssetUrls: {
+//           base: null,
+//           includeAbsolute: false,
+//         },
+//       },
+//     }),
+//   ],
 // });
