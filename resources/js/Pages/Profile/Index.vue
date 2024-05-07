fea19109e5c82581d233fcdfd01e2583b42288a3
AKO PRECIOUS <akoprecious58@gmail.com>
2024-05-06 22:13:53 -0400

    show

--- resources/js/Pages/Profile/Index.vue
+++ resources/js/Pages/Profile/Index.vue
@@ -3,40 +3,81 @@
 <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
 <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
 
-<section class="pt-16 bg-blueGray-50">
-<div class="w-full lg:w-9/12 px-4 mx-auto">
-  <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16">
+<section class="pt-16 bg-snow dark:bg-oynx">
+<div class=" lg:w-4/5 px-4 mx-auto">
+  <div class="relative flex flex-col min-w-0 break-words shadow-reverse w-full mb-6  rounded-lg mt-16">
     <div class="px-6">
       <div class="flex flex-wrap justify-center">
-        <div class="w-full px-4 flex justify-center">
-          <div class="relative">
-            <img alt="..." src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg" class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px">
+        <div class="w-full px-4 flex lg:hidden justify-center lg:justify-between gap-4">
+            <div class="">
+              <div class="relative w-full   ">
+                <img alt="..." src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg" class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px">
+              </div>
+
           </div>
         </div>
-        <div class="w-full px-4 text-center mt-20">
+        <div class="w-full hidden px-4 lg:flex justify-between gap-4">
+            <div class=" lg:flex py-4 lg:pt-4 lg:w-4/12 ">
+            <div class=" p-3 text-center">
+              <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+                222
+              </span>
+              <span class="text-sm text-blueGray-400">Days Cooking</span>
+            </div>
+            <div class=" p-3  text-center">
+              <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+                4.5
+              </span>
+              <span class="text-sm text-blueGray-400">Rating</span>
+            </div>
+            <div class=" p-3 text-center">
+              <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+                89
+              </span>
+              <span class="text-sm text-blueGray-400">Review(s)</span>
+            </div>
+          </div>
+          <div class="">
+              <div class="relative w-full   ">
+                <img alt="..." src="https://demos.creative-tim.com/notus-js/assets/img/team-2-800x800.jpg" class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px">
+              </div>
+
+          </div>
+          <div class=" hidden lg:block text-center lg:w-4/12 mt-6">
+        <h3 class="text-xl font-semibold leading-normal mb-2 text-blueGray-700 ">
+          Jenna Stones
+        </h3>
+        <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+          <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
+          Los Angeles, California
+        </div>
+        
+      </div>
+        </div>
+        <div class="block lg:hidden w-full px-4 text-center mt-20">
           <div class="flex justify-center py-4 lg:pt-4 pt-8">
             <div class="mr-4 p-3 text-center">
               <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
                 22
               </span>
-              <span class="text-sm text-blueGray-400">Friends</span>
+              <span class="text-sm text-blueGray-400">Months Cooking</span>
             </div>
             <div class="mr-4 p-3 text-center">
               <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-                10
+                4.5
               </span>
-              <span class="text-sm text-blueGray-400">Photos</span>
+              <span class="text-sm text-blueGray-400">Rating</span>
             </div>
             <div class="lg:mr-4 p-3 text-center">
               <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
                 89
               </span>
-              <span class="text-sm text-blueGray-400">Comments</span>
+              <span class="text-sm text-blueGray-400">Review(s)</span>
             </div>
           </div>
         </div>
       </div>
-      <div class="text-center mt-12">
+      <div class="block lg:hidden text-center mt-6">
         <h3 class="text-xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
           Jenna Stones
         </h3>
@@ -53,7 +94,7 @@
           University of Computer Science
         </div>
       </div>
-      <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
+      <div class="mt-10 py-10 border-t  border-gray-500 text-center">
         <div class="flex flex-wrap justify-center">
           <div class="w-full lg:w-9/12 px-4">
             <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
@@ -72,16 +113,5 @@
     </div>
   </div>
 </div>
-<footer class="relative  pt-8 pb-6 mt-8">
-  <div class="container mx-auto px-4">
-    <div class="flex flex-wrap items-center md:justify-between justify-center">
-      <div class="w-full md:w-6/12 px-4 mx-auto text-center">
-        <div class="text-sm text-blueGray-500 font-semibold py-1">
-          Made with <a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank">Notus JS</a> by <a href="https://www.creative-tim.com" class="text-blueGray-500 hover:text-blueGray-800" target="_blank"> Creative Tim</a>.
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
 </section>
 </template>
