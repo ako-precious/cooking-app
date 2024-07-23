502e587db4f34babbe0e2345917fa8784a6d8c39
AKO PRECIOUS <akoprecious58@gmail.com>
2024-07-23 00:02:20 -0400

    set the mene well

---
+++ resources/js/Layouts/Disclaimer.vue
@@ -0,0 +1,109 @@
+<template>    
+    <div
+    id="defaultModal"
+    tabindex="-1"
+    aria-hidden="true"
+    class=" overflow-y-auto overflow-x-hidden flex justify-center items-center fixed top-0 right-0 left-0 w-full md:inset-0 h-modal md:h-full z-990"
+    >
+    <div class="relative p-4  w-full m-auto max-w-2xl h-full md:h-auto">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div
+                class="flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600"
+            >
+                <h3
+                    class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white"
+                >
+                    Terms of Service
+                </h3>
+                <button
+                    type="button"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-toggle="defaultModal"
+                >
+                    <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            fill-rule="evenodd"
+                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                            clip-rule="evenodd"
+                        ></path>
+                    </svg>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="h-52 overflow-y-auto p-5 space-y-6">
+                <p
+                    class="text-sm leading-relaxed text-gray-600 dark:text-gray-400"
+                >
+                    Ounjemi is a student-run cooking platform that connects
+                    home cooks with customers. Please be aware that meals
+                    prepared by our student cooks are typically made in
+                    their personal homes, which may not have been inspected
+                    by public health officials. However, we ensure that all
+                    our cooks hold a valid Food Handling Certificate,
+                    demonstrating their understanding of proper food
+                    handling and safety practices.
+                </p>
+                <p
+                    class="text-sm leading-relaxed text-gray-600 dark:text-gray-400"
+                >
+                    While we take steps to ensure the quality and safety of
+                    our meals, we want to transparently disclose this
+                    information to our customers. Additionally, we encourage
+                    customers to rate their meals after each order, helping
+                    us maintain high standards and accountability within our
+                    community.
+                </p>
+                <p
+                    class="text-sm leading-relaxed text-gray-600 dark:text-gray-400"
+                >
+                    Please note that our platform is in its early stages,
+                    and many of our student cooks share apartments, making
+                    it challenging to meet traditional commercial kitchen
+                    inspection standards. We appreciate your understanding
+                    and support as we grow and evolve.
+                </p>
+                <p
+                    class="text-sm leading-relaxed text-gray-600 dark:text-gray-400"
+                >
+                    Please note that our platform is in its early stages,
+                    and many of our student cooks share apartments, making
+                    it challenging to meet traditional commercial kitchen
+                    inspection standards. We appreciate your understanding
+                    and support as we grow and evolve.
+                </p>
+                <p
+                    class="text-sm leading-relaxed text-gray-600 dark:text-gray-400"
+                >
+                We encourage you to read our disclaimer carefully before placing your order with Ounjemi. By proceeding, you confirm your understanding and acceptance of the terms."
+                </p>
+            </div>
+            <!-- Modal footer -->
+            <div
+                class="flex items-center p-5 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600"
+            >
+                <button
+                    data-modal-toggle="defaultModal"
+                    type="button"
+                    class="text-white bg-gradient-to-br from-pink-500 to-voilet-500 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 text-center inline-flex items-center shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform"
+                >
+                    I accept
+                </button>
+                <button
+                    data-modal-toggle="defaultModal"
+                    type="button"
+                    class="text-gray-600 bg-white hover:bg-gray-100 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                >
+                    Decline
+                </button>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
--- resources/js/Pages/Cook/Header.vue
+++ resources/js/Pages/Cook/Header.vue
@@ -1,54 +1,106 @@
 <script setup>
 import { Head, Link } from "@inertiajs/vue3";
 
-import ApplicationMark from '@/Components/ApplicationMark.vue';
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import ApplicationMark from "@/Components/ApplicationMark.vue";
 import SettingsDropdown from "@/Components/SettingsDropdown.vue";
+
 </script>
 
 <template>
     <div class="md:px-5">
-
-        <header class="py-2  container  bg-snow dark:bg-oynx z-990 transition-all duration-300 delay-75 ease-in animate-fade-in">
+        <header
+            class="py-2 container bg-snow dark:bg-oynx z-990 transition-all duration-300 delay-75 ease-in animate-fade-in"
+        >
             <div
-                :class="{ 'fix align-bottom shadow-sm' : isHeaderFixed }"
+                :class="{ 'fix align-bottom shadow-sm': isHeaderFixed }"
                 class="flex justify-between items-center w-full bg-snow dark:bg-oynx"
             >
-            <Link :href="route('welcome')">
-             <div class="w-full flex items-center">
-                    <ApplicationMark class="block w-auto" />
-                    <span
-                        class="hidden md:block ml-2 font-bold transition-all duration-200 ease-in-out text-2xl uppercase text-oynx dark:text-snow"
-                    >
-                        Ounjemi
-                    </span>
-                </div>
-            </Link>
-    
+                <Link :href="route('welcome')">
+                    <div class="w-full flex items-center">
+                        <ApplicationMark class="block w-auto" />
+                        <span
+                            class="hidden md:block ml-2 font-bold transition-all duration-200 ease-in-out text-2xl uppercase text-oynx dark:text-snow"
+                        >
+                            Ounjemi
+                        </span>
+                    </div>
+                </Link>
+
                 <div
-                    class="bg-transparent  w-4/12 sm:w-4/12 capitalize font-bold text-sm mr-4 flex justify-around  transition-all duration-300 delay-75 ease-in"
+                    class=" hidden  bg-transparent w-4/12 sm:w-4/12 capitalize font-bold text-sm mr-4 sm:flex justify-around transition-all duration-300 delay-75 ease-in"
                     name=""
                     id=""
                 >
-                    <Link :href="`/cook/order`" class="py-2 px-3 navbar-link" >
+                    <Link :href="`/cook/order`" class="py-2 px-3 navbar-link">
                         <p>Orders</p>
                     </Link>
-                    <Link class="py-2 md:px-3 navbar-link" :href="`/cook/calendar`">
+                    <Link
+                        class="py-2 md:px-3 navbar-link"
+                        :href="`/cook/calendar`"
+                    >
                         <p>Calendar</p>
                     </Link>
-                    <Link :href="`/cook/menu`" class="py-2 px-3 navbar-link" >
+                    <Link :href="`/cook/menu`" class="py-2 px-3 navbar-link">
                         <p>Menu</p>
                     </Link>
-                    <!-- <Link class="py-2 px-3 navbar-link flex items-center" href="" >
-                        <p>More  </p> <font-awesome-icon
-                                            icon="caret-down"
-                                            class="h-3 lg:h-4 ml-2 svg-inline--fa fa-user fa-w-14 fa-9x"></font-awesome-icon>
-                                          
-                                       
-                    </Link> -->
+                   
                 </div>
-                <div class="flex sm:items-center sm:ms-6">
-                    <!-- Settings Dropdown -->
-              <SettingsDropdown></SettingsDropdown>
+                <div class="flex">
+
+                    <div class="flex sm:items-center sm:ms-6">
+                        <!-- Settings Dropdown -->
+                        <SettingsDropdown></SettingsDropdown>
+                    </div>
+                    <div class=" ml-6 sm:hidden flex items-center">
+                        <a
+                            mini-sidenav-burger=""
+                            href="javascript:;"
+                            class="sm:marker:hidden p-0 text-sm text-white transition-all ease-nav-brand block"
+                            aria-expanded="false"
+                        >
+                            <Dropdown align="right" width="48">
+                                <template #trigger>
+                                    <div class="w-4.5 overflow-hidden">
+                                        <i
+                                            class="ease mb-0.75 relative block h-0.5 rounded-sm bg-persian group-hover:bg-polynesian dark:group-hover:bg-lighred transition-all"
+                                        ></i>
+                                        <i
+                                            class="ease mb-0.75 relative block h-0.5 rounded-sm bg-persian group-hover:bg-polynesian dark:group-hover:bg-lighred transition-all"
+                                        ></i>
+                                        <i
+                                            class="ease relative block h-0.5 rounded-sm bg-persian group-hover:bg-polynesian dark:group-hover:bg-lighred transition-all"
+                                        ></i>
+                                    </div>
+                                </template>
+    
+                                <template #content class="bg-snow dark:bg-oynx">
+                                    <div
+                                        class="origin-top-right absolute right-0 mt-2 w-48 delay-75 rounded-md border-snow ring-1 bg-gradient-to-br from-[#e3dedf] to-[#ffffff] -shadow-snow-sm hover:shadow-snow-sm dark:bg-gradient-to-br dark:from-[#2b312e] dark:to-[#333a37] dark:-shadow-oynx-sm hover:dark:shadow-oynx-sm z-20 transition-all duration-250 ease-in dark:border-oynx"
+                                    >
+                                        <DropdownLink
+                                        :href="`/cook/order`" 
+                                            class="flex w-full items-center rounded-md px-4 py-2 text-sm text-oynx hover:text-polynesian dark:text-snow dark:hover:text-lighred cursor-pointer transition-all duration-200 ease-in-out"
+                                        ><p>Orders</p>
+                                        </DropdownLink>
+                                        <DropdownLink
+                                            :href="`/cook/calendar`"
+                                            class="relative flex items-center rounded-md px-4 py-2 text-sm text-oynx hover:text-polynesian dark:text-snow dark:hover:text-lighred cursor-pointer transition-all duration-200 ease-in-out"
+                                        >
+                                        <p>Calendar</p>
+                                        </DropdownLink>
+                                        <DropdownLink
+                                        :href="`/cook/menu`" 
+                                            class="relative flex items-center rounded-md px-4 py-2 text-sm text-oynx hover:text-polynesian dark:text-snow dark:hover:text-lighred cursor-pointer transition-all duration-200 ease-in-out"
+                                        >
+                                        <p>Menu</p>
+                                        </DropdownLink>
+                                    </div>
+                                </template>
+                            </Dropdown>
+                        </a>
+                    </div>
                 </div>
             </div>
         </header>
