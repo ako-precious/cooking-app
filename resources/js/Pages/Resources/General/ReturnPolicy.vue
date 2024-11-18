f88db27573c464c4e23af2a0c858a4665c6951fc
AKO PRECIOUS <akoprecious58@gmail.com>
2024-11-17 20:30:49 -0500

    I am trying to figure out how I can make a table

--- resources/js/Pages/Resources/General/ReturnPolicy.vue
+++ resources/js/Pages/Resources/General/ReturnPolicy.vue
@@ -1,149 +1,193 @@
 <script setup>
-import { onMounted } from "vue";
-import { Head, Link, usePage } from "@inertiajs/vue3";
+import { Head, Link } from "@inertiajs/vue3";
+
 import Footer from "@/Layouts/Footer.vue";
-import axios from "axios";
-// import { subscribeUserToPush } from "/resources/js/bootstrap.js"; // Adjust the path as necessary
-import { ref, computed } from "vue";
-
-
-
-defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
-    // pushSub: Boolean,
-});
-
-// const notificationPermission = ref(Notification.permission);
-
-// const showEnableButton = computed(
-//     () => notificationPermission.value !== "granted"
-// );
-
-// async function requestNotificationPermission() {
-//     const permission = await Notification.requestPermission();
-
-//     if (permission === "granted") {
-//         await subscribeUserToPush();
-//         notificationPermission.value = permission;
-//     } else {
-//         alert(
-//             "You need to enable notifications in your browser settings to receive push notifications."
-//         );
-//     }
-// }
-</script>
-
-<script>
-export default {
-    inheritAttrs: false,
-
-    data() {
-        return {
-            meals: [],
-            page: 1, // Current page
-            perPage: 12, // Number of items per page
-            hasMoreData: true,
-            isHeaderFixed: false,
-        };
-    },
-    beforeDestroy() {
-       
-    },
-
-    mounted() {
-      
-    },
-    created() {
-       
-    },
-    methods: {
-       
-       
-    },
-};
+import HeaderVue from "../Header.vue";
 </script>
 
 <template>
-    <Head title="Welcome" />
-    <!-- component -->
-    
+    <Head title="Menu" />
     <div
-        class="container p-4 lg:p-10 mx-auto relative grid grid-cols-1 gap-8 sm:items-center min-h-screen selection:bg-red-500 selection:text-white bg-snow dark:bg-oynx"
-    >mnnnnnnnnnnnnnn
-        
-    </div>
-   
+        class="relative sm:flex sm:justify-center sm:items-center bg-center bg-snow dark:bg-oynx selection:bg-red-500 selection:text-white"
+    >
+        <div class="container relative mx-auto overflow-hidden">
+            <HeaderVue> </HeaderVue>
 
+            <div
+                class="m-auto flex flex-col justify-center h-full p-6 lg:pb-20 w-full"
+            >
+                <div class="lg:w-full">
+                    <div class="flex flex-col w-full">
+                     
+                        
+                        <div
+                            class="flex flex-col md:flex-row items-start justify-between md:items-center"
+                        >
+                            <div class="py-3">
+                                <h1
+                                    class="font-semibold text-xl lg:text-2xl text-oynx dark:text-snow"
+                                >
+                                    You have meal(s) in your
+                                    menu
+                                </h1>
+                            </div>
+                            <div
+                                class="flex flex-wrap md:flex-row items-start justify-between md:items-center"
+                            >
+                                <div class="p-2 flex">
+                                    <Link :href="`/become-a-cook/overview`">
+                                        <button
+                                            class="shadow-extra-small rounded-md cursor-pointer w-[150px] h-[50px] text-oynx dark:text-snow font-semibold border-none flex justify-center items-center"
+                                        >
+                                            <span
+                                                class="span-mother flex overflow-hidden"
+                                            >
+                                                <span>Cr</span>
+                                                <span>ea</span>
+                                                <span>t</span>
+                                                <span> e M</span>
+                                                <span>ea</span>
+                                                <span
+                                                    >l
+                                                    <font-awesome-icon
+                                                        icon="plus"
+                                                /></span>
+                                            </span>
+                                            <span
+                                                class="span-mother2 text-persian"
+                                            >
+                                                <span>Cr</span>
+                                                <span>ea</span>
+                                                <span>t</span>
+                                                <span> e M</span>
+                                                <span>ea</span>
+                                                <span
+                                                    >l
+                                                    <font-awesome-icon
+                                                        icon="plus"
+                                                /></span>
+                                            </span></button
+                                    ></Link>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    </div>
+                </div>
+            </div>
+
+            <div
+        class="container p-4 lg:p-10 mx-auto relative grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 sm:items-center min-h-screen selection:bg-red-500 selection:text-white bg-snow dark:bg-oynx"
+    >
+        <div v-for="meal in meals" :key="meal.id" class="animate-fade-in"> 
+
+            <FoodCard :meal="meal"></FoodCard>
+        </div>
+    </div>
+    <div
+        class="flex justify-center items-center flex-col transition-all duration-250 delay-75 ease-bounce sha"
+        v-if="hasMoreData"
+    >
+        <button @click="loadMoreData" class="button">Show More</button>
+    </div>
+    <!-- {{ $page.props.auth.user }} -->
+   
     <Footer></Footer>
+        </div>
+    </div>
 </template>
 
+<script>
+import axios from "axios";
+
+export default {
+    
+    
+};
+</script>
+
 <style scoped>
-.button {
-    --color: #1b998b;
-    font-family: inherit;
-    display: inline-block;
-    width: 8em;
-    height: 2.6em;
-    line-height: 2.5em;
-    margin: 10px;
-    position: relative;
-    overflow: hidden;
-    border: 2px solid var(--color);
-    transition: color 0.5s;
-    z-index: 1;
-    font-size: 17px;
-    border-radius: 6px;
-    font-weight: 600;
-    color: var(--color);
-}
-
-.button:before {
-    content: "";
+button:hover .span-mother {
     position: absolute;
-    z-index: -1;
-    background: var(--color);
-    height: 150px;
-    width: 200px;
-    border-radius: 50%;
 }
 
-.button:hover {
-    color: #fff;
+button:hover .span-mother span {
+    transform: translateY(1.3em);
 }
 
-.button:before {
-    top: 100%;
-    left: 100%;
-    transition: all 0.7s;
+button .span-mother span:nth-child(1) {
+    transition: 0.2s;
 }
 
-.button:hover:before {
-    top: -30px;
-    left: -30px;
+button .span-mother span:nth-child(2) {
+    transition: 0.3s;
 }
 
-.button:active:before {
-    background: #0e534b;
-    transition: background 0s;
-}
-.bg-dots-darker {
-    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
+button .span-mother span:nth-child(3) {
+    transition: 0.4s;
 }
 
+button .span-mother span:nth-child(4) {
+    transition: 0.5s;
+}
+
+button .span-mother span:nth-child(5) {
+    transition: 0.6s;
+}
+
+button .span-mother span:nth-child(6) {
+    transition: 0.7s;
+}
+button .span-mother2 {
+    display: flex;
+    position: absolute;
+    overflow: hidden;
+}
+
+button .span-mother2 span {
+    transform: translateY(-1.5em);
+}
+
+button:hover .span-mother2 span {
+    transform: translateY(0);
+}
+
+button .span-mother2 span {
+    transition: 0.2s;
+}
+
+button .span-mother2 span:nth-child(2) {
+    transition: 0.3s;
+}
+
+button .span-mother2 span:nth-child(3) {
+    transition: 0.4s;
+}
+
+button .span-mother2 span:nth-child(4) {
+    transition: 0.5s;
+}
+
+button .span-mother2 span:nth-child(5) {
+    transition: 0.6s;
+}
+
+button .span-mother2 span:nth-child(6) {
+    transition: 0.7s;
+}
 
 @keyframes fade-in {
     from {
         opacity: 0;
+        bottom: -10rem;
     }
     to {
         opacity: 1;
+        bottom: 0;
     }
 }
 
 .animate-fade-in {
-    animation: fade-in 0.3s ease-in;
+    animation: fade-in 0.5s ease-in;
 }
 </style>
---
+++ resources/js/Pages/Resources/Header.vue
@@ -0,0 +1,149 @@
+<script setup>
+import { Head, Link } from "@inertiajs/vue3";
+
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import ApplicationMark from "@/Components/ApplicationMark.vue";
+import SettingsDropdown from "@/Components/SettingsDropdown.vue";
+
+</script>
+
+<template>
+    <div class="md:px-5">
+        <header
+            class="py-2 container bg-snow dark:bg-oynx z-990 transition-all duration-300 delay-75 ease-in animate-fade-in"
+        >
+            <div
+                :class="{ 'fix align-bottom shadow-sm': isHeaderFixed }"
+                class="flex justify-between items-center w-full bg-snow dark:bg-oynx"
+            >
+                <Link :href="route('welcome')">
+                    <div class="w-full flex items-center">
+                        <ApplicationMark class="block w-auto" />
+                        <span
+                            class="hidden md:block ml-2 font-bold transition-all duration-200 ease-in-out text-2xl uppercase text-oynx dark:text-snow"
+                        >
+                            Ounjemi
+                        </span>
+                    </div>
+                </Link>
+
+                <div
+                    class=" hidden  bg-transparent w-4/12 sm:w-4/12 capitalize font-bold text-sm mr-4 sm:flex justify-around transition-all duration-300 delay-75 ease-in"
+                    name=""
+                    id=""
+                >
+                    <Link :href="`/cook/order`" class="py-2 px-3 navbar-link">
+                        <p>Orders</p>
+                    </Link>
+                    <Link
+                        class="py-2 md:px-3 navbar-link"
+                        :href="`/cook/calendar`"
+                    >
+                        <p>Calendar</p>
+                    </Link>
+                    <Link :href="`/cook/menu`" class="py-2 px-3 navbar-link">
+                        <p>Menu</p>
+                    </Link>
+                    <!-- <Link :href="`/cook/menu`" class="py-2 px-3 navbar-link">
+                        <p>More  <font-awesome-icon class="ml-1"
+                                                        icon="caret-down"
+                                                /></p>
+                    </Link> -->
+                    <div class="py-2 px-3 navbar-link">
+                        <!-- class="sm:marker:hidden p-0 text-sm text-white transition-all ease-nav-brand block" -->
+                        <a
+                            mini-sidenav-burger=""
+                            href="javascript:;"
+                            aria-expanded="false"
+                        >
+                            <Dropdown align="right" width="48">
+                                <template #trigger>
+                                    <p>More  <font-awesome-icon class="ml-1"
+                                                        icon="caret-down"
+                                                /></p>
+                                </template>
+    
+                                <template #content class="bg-snow dark:bg-oynx">
+                                    <div
+                                        class="origin-top-right absolute right-0 mt-2 w-48 delay-75 rounded-md border-snow ring-1 bg-gradient-to-br from-[#e3dedf] to-[#ffffff] -shadow-snow-sm hover:shadow-snow-sm dark:bg-gradient-to-br dark:from-[#2b312e] dark:to-[#333a37] dark:-shadow-oynx-sm hover:dark:shadow-oynx-sm z-20 transition-all duration-250 ease-in dark:border-oynx"
+                                    >
+                                        <DropdownLink
+                                        :href="`/cook/account`"
+                                            class="flex w-full items-center rounded-md px-4 py-2 text-sm text-oynx hover:text-polynesian dark:text-snow dark:hover:text-lighred cursor-pointer transition-all duration-200 ease-in-out"
+                                        ><p>Payment Account</p>
+                                        </DropdownLink>
+                                        <DropdownLink
+                                            :href="`/onboarding/verify-your-account`"
+                                            class="relative w-full text-nowrap flex items-center rounded-md px-4 py-2 text-sm text-oynx hover:text-polynesian dark:text-snow dark:hover:text-lighred cursor-pointer transition-all duration-200 ease-in-out"
+                                        >
+                                        <p>Verification Status</p>
+                                        </DropdownLink>
+                                    </div>
+                                </template>
+                            </Dropdown>
+                        </a>
+                    </div>
+                   
+                </div>
+                <div class="flex">
+
+                    <div class="flex sm:items-center sm:ms-6">
+                        <!-- Settings Dropdown -->
+                        <!-- <SettingsDropdown></SettingsDropdown> -->
+                    </div>
+                 
+                </div>
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    data() {
+        return {
+            isHeaderFixed: false,
+        };
+    },
+    mounted() {
+        window.addEventListener("scroll", this.handleScroll);
+    },
+    beforeDestroy() {
+        window.removeEventListener("scroll", this.handleScroll);
+    },
+
+    methods: {
+        handleScroll() {
+            // Adjust the scroll threshold as needed
+            const scrollThreshold = 20;
+            this.isHeaderFixed = window.scrollY > scrollThreshold;
+        },
+        truncatedIng(description) {
+            // Check if description exists and has more than 30 characters
+            if (description && description.length > 3) {
+                // Truncate description to 30 characters and add ellipsis
+                return description.slice(0, 3) + "...";
+            } else {
+                // Return full description if it's less than or equal to 30 characters
+                return description;
+            }
+        },
+    },
+};
+</script>
+
+<!-- Saving for later -->
+<!-- "Indulge your taste buds and unlock a world of culinary delights with our platform, where passionate home chefs transform ordinary ingredients into extraordinary experiences. From tantalizing dishes to mouthwatering desserts, our community of food artisans invites you to savor their creations and bring the flavors of the world into your home. Whether you're seeking a gourmet feast for a special occasion or craving everyday comfort food made with love, discover your perfect match among our talented cooks who are ready to turn your cravings into unforgettable culinary adventures." -->
+
+<style scoped>
+.fix {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 100vw;
+    padding: 1rem 3rem;
+    z-index: 1000; /* Adjust z-index as needed */
+}
+</style>
